5d812ceff2438f73488b310e0a9c9a4e8d1f020d
Kirill Begej <fleki679@yandex.ru>
2024-10-27 11:26:27 +0300

    refactor: подготовить index.html в валидации, подключить index.js, вынести селекторы форм в constants.js

---
+++ js/utils/constants.js
@@ -0,0 +1,10 @@
+export const validationConfig = {
+  formSelector: '.form',
+  inputLabelSelector: '.input',
+  inputSelector: '.input__element',
+  checkboxSelector: '.checkbox__element',
+  buttonSelector: 'button',
+  inputErrorClass: 'input_event_error',
+  inputSuccessClass: 'input_event_success',
+  inactiveButtonClass: 'button_inactive'
+};
