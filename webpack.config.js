207061d062251df406f257fab7df5347da9c6520
Kirill Begej <fleki679@yandex.ru>
2024-11-07 14:09:55 +0300

    feat: установить и выполнить первоначальную настройку typescript, ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
