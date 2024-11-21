3b674d7811244357e4cc4572765b1efa813e87d0
Kirill Begej <fleki679@yandex.ru>
2024-11-21 21:03:13 +0300

    feat: добавить NotFoundPage

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './NotFoundPage.module.css'
+
+interface NotFoundPageProps {
+  className?: string
+}
+
+export const NotFoundPage: FC<NotFoundPageProps> = ({ className }) => (
+  <div className={classNames(cls.notfoundpage, {}, [className])}>
+    <h2 className={cls.title}>
+      <span className={cls.titleText}>404 </span>
+      <span className={cls.titleImage}>&#128577;</span>
+    </h2>
+    <p className={cls.text}>Упс! Эту страницу невозможно найти!</p>
+  </div>
+)
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -2,17 +2,20 @@ import { RouteProps } from 'react-router-dom'
 import { MainPage } from 'pages/MainPage'
 import { ResultPage } from 'pages/ResultPage'
 import { SettingsPage } from 'pages/SettingsPage'
+import { NotFoundPage } from 'pages/NotFoundPage'
 
 export enum AppRoutes {
   MAIN = 'main',
   RESULT = 'result',
-  SETTINGS = 'settings'
+  SETTINGS = 'settings',
+  NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.RESULT]: '/result',
-  [AppRoutes.SETTINGS]: '/settings'
+  [AppRoutes.SETTINGS]: '/settings',
+  [AppRoutes.NOT_FOUND]: '*'
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -27,5 +30,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.SETTINGS]: {
     path: RoutePath.settings,
     element: <SettingsPage />
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />
   }
 }
